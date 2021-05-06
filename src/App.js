5778ff5402773db64d1a6ee8e1997c63e8fb5fff
Mohammad Altamimi <m98altamimi@gmail.com>
2021-05-06 18:50:55 +0300

    lab07-fix-alert

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 import Info from './components/Info';
 import Map from './components/Map';
 import WeatherData from './components/WeatherData';
+import Alert from './components/AlertComp';
 
 
 
@@ -15,9 +16,11 @@ export class App extends Component {
     super(props);
     this.state = {
       searchField: '',
-      locationData:'',
-      show:false,     // this flag controls when the image and the info show
-      weatherData:[],
+      locationData: '',
+      show: false,     // this flag controls when the image and the info show
+      weatherData: [],
+      alertDisplay: false,
+      errorMessage: ''
     }
   }
 
@@ -26,49 +29,103 @@ export class App extends Component {
 
 
   getLocation = async (e) => {
-    e.preventDefault();
-    const locationUrl=`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MAP_KEY}&q=${this.state.searchField}&format=json`;
-    const locationRequest=await axios.get(locationUrl);
+    try {
+      if (this.state.searchField !== '') {
+        e.preventDefault();
+        const locationUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_MAP_KEY}&q=${this.state.searchField}&format=json`;
+        const locationRequest = await axios.get(locationUrl);
+        this.setState({
+          locationData: locationRequest.data[0],
+        })
+        this.getWeather();
+      } else {
+        this.setState({
+          errorMessage: 'Please enter a valid city name.',
+          alertDisplay: true,
+          locationData: '',
+          show: false,
+          weatherData: []
+        })
+      }
+    }
+    catch (error) {
+      this.setState({
+        alertDisplay: true,
+        errorMessage: error,
+      })
+    }
 
-    const weatherUrl=`${process.env.REACT_APP_SERVER}/weather`;
-    const weatherRequest= await axios.get(weatherUrl);
-    console.log(weatherRequest); // we found out that it is an array so we need to loop through the component that uses this info
 
-    // console.log(locationRequest); /// to check if there's an error in the request
-    this.setState({
-      locationData:locationRequest.data[0],
-      show:true,
-      weatherData:weatherRequest.data,
-
-    })
-
-    console.log(this.state.weatherData); // to check if its assigned to the new state.
 
   }
 
 
 
-  
 
-  reloadPage = (event) => window.location.reload();
+
+  getWeather = async () => {
+
+    const weatherUrl = `${process.env.REACT_APP_SERVER}/weather`;
+    const weatherRequest = await axios.get(weatherUrl);
+
+
+    this.setState({
+      show: true,
+      weatherData: weatherRequest.data,
+    })
+
+  }
+
+
+
+
+
+
+  reloadPage = (event) => {
+    window.location.reload();
+    this.setState({
+      alertDisplay: false,
+      errorMessage: '',
+    })
+  }
 
 
   updateSearchField = (e) => {
-    this.setState({ searchField:e.target.value });
-    console.log(e.target.value);
-    
+    this.setState({
+      searchField: e.target.value
+    });
+
   }
 
-  
+
+  onClose = () => {
+    this.setState({ alertDisplay: false,
+    errorMessage:'', })
+  }
+
+
   render() {
     return (
       <div>
         <Header />
         <br />
         <Form updateSearchField={this.updateSearchField} getLocation={this.getLocation} reloadPage={this.reloadPage} />
-        {this.state.show === true && <><Info name={this.state.locationData.display_name}/>
-        <Map lat={this.state.locationData.lat} lon={this.state.locationData.lon} /><WeatherData weatherInfo={this.state.weatherData}/> </>}
-        
+        {this.state.show === true ?
+          <>
+            <Info name={this.state.locationData.display_name} />
+            <Map lat={this.state.locationData.lat} lon={this.state.locationData.lon} />
+            <WeatherData weatherInfo={this.state.weatherData} />
+          </>
+          :
+          <>
+            <Alert alertDisplay={this.state.alertDisplay} errorMessage={this.state.errorMessage} onClose={this.onClose} />
+          </>
+        }
+
+
+
+
+
         <Footer />
       </div>
     )
