34deee8ab4ff3292e07037c8ed75367ed9bf080d
Mohammad Altamimi <m98altamimi@gmail.com>
2021-05-08 13:09:46 +0300

    some styling

--- src/components/MovieData.js
+++ src/components/MovieData.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 
+
+
 export class MovieData extends Component {
   render() {
     return (
 
 
       this.props.movieInfo.map((data, index) => {
-        return <Card key={index}>
-          <Card.Img variant="top" width={'20'} src={data.image}/>
-          <Card.Body>
-            <Card.Title>{data.title}</Card.Title>
-            <Card.Text>
-              Release date: {data.releaseDate}<br/>
+        return <>
+          <Card key={index} style={{ width: '18rem' }}>
+            <Card.Img variant="top" width={50} height={250} src={data.image} />
+            <Card.Body>
+              <Card.Title>{data.title}</Card.Title>
+              <Card.Text>
+              Release date: {data.releaseDate}<br />
               Rating: {data.rating}
-            </Card.Text>
-          </Card.Body>
-        </Card>;
+              </Card.Text>
+            </Card.Body>
+          </Card>;
+          <br/>
+        </>;
       }
 
 
 
 
 
-      ) );
+      ));
   }
 }
 
--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Card, CardDeck } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 export class WeatherData extends Component {
   render() {
     return (this.props.weatherInfo.map((data, index) => {
-      return <div key={index}><CardDeck>
-        <Card bg={'primary'} text={'light'} border={'danger'} >
-          <Card.Body>
+      return <div key={index}>
+        <Card style={{ width: '18rem' }} bg={'primary'} text={'light'} border={'danger'} >
+          <Card.Body width={250} height={250} >
             <Card.Title>Weather Summary 🌧️ ☀️ 🌈</Card.Title>
             <Card.Text>
                Date recorded: {data.date} <br/>
@@ -14,8 +14,9 @@ export class WeatherData extends Component {
             </Card.Text>
           </Card.Body>
         </Card>
+        <br/>
 
-      </CardDeck>;
+
       </div>;
     })
 
